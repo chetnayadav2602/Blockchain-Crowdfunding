90d16fdd8c829224fc834ed953bfe5998e823b4d
Anurag Gupta <anurag.gupta@utexas.edu>
2022-04-24 19:36:05 -0500

    Created API integration for Create Campaign

--- Crowdfunding_Client/src/app/campaign-factory/campaign-factory.component.ts
+++ Crowdfunding_Client/src/app/campaign-factory/campaign-factory.component.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 
@@ -11,19 +11,30 @@ import { Component, OnInit } from '@angular/core';
 export class CampaignFactoryComponent implements OnInit {
 
   constructor(private http: HttpClient) { }
-
+  data2;
   ngOnInit(): void {
     
+    
   }
-  SubmitDetails(postCampForm){  
+
+  SubmitDetails(postCampForm: any){  
     console.log(postCampForm); 
     this.postContracts(postCampForm);
      
   } 
 
   
-  public postContracts(postCampForm){
-    this.http.post("http://localhost:5000/postCampaings",postCampForm);
+  public postContracts(postCampForm: any){
+    
+    const baseURL = 'http://localhost:5000/postCampaings'
+    const headers = {'content-type':'application/json'};
+    const body=JSON.stringify({ cname: postCampForm.cname, cdesc: postCampForm.cdesc, cimage : postCampForm.cimage });
+    const params = new HttpParams().set('cname', postCampForm.cname).set('cdesc',postCampForm.cdesc).set('cimage', postCampForm.cimage);
+    console.log(params);
+    this.http.post(baseURL, body,{'headers':headers, 'params': params}).subscribe(data => {
+      this.data2 = data;
+      alert('Campaign Posted!!')
+    })
   }
 
 
---
+++ Crowdfunding_Client/src/app/send-kyc/send-kyc.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-send-kyc',
+  templateUrl: './send-kyc.component.html',
+  styleUrls: ['./send-kyc.component.css']
+})
+export class SendKYCComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
